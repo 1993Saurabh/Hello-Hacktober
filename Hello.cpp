40298d5664286970dafb6d2bb2e9316084282a70
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2018-10-13 20:25:05 +0530

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cout<<"Hello World"<<endl;
+  cout<<"Start with c++"<<endl;
+  cout<<"Learn basic syntax";
+  
+  // if-else statement
+  int x = 18;
+  int y = 25;
+  if(x>=x)
+  {
+    cout<<"Adult"<<endl;
+  }
+  else
+  {
+    cout<<"Child"<<endl;
+  }
+  
+  //for loop
+  
+  for(int i=1;i<=100;i++)
+  {
+    cout<<i<<endl;            //print 1 to 100 in a coulmn;
+  }
+  
+  cout<<"Thanks"<<endl;
+  return 0;
+}
