741ccfce66cb8583af5988863111833c532d7b9c
samarthamahesh <42439398+samarthamahesh@users.noreply.github.com>
2018-10-26 22:10:10 +0530

    Update H3ll0_w0r4.c

--- Languages/H3ll0_w0r4.c
+++ Languages/H3ll0_w0r4.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
+# include <stdio.h>
  
-              int main() {
+              int main(void) {
  
                printf("Hello, World!");
                return 0;
-               }
 }
