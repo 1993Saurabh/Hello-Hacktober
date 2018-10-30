d74b27dde7cab5582e8906e0a94790b5ae79115e
Sohil Ladhani <sohilladhani@gmail.com>
2018-10-30 18:28:52 +0530

    Created NamasteDuniya.java for hacktoberfest

---
+++ NamasteDuniya.java
@@ -0,0 +1,6 @@
+public class NamasteDuniya {
+	public static void main(String[] args) {
+		/* "Hello world" roughly translates to "Namaste Duniya" in Hindi */
+		System.out.println("Namaste, Duniya");
+	}
+}
