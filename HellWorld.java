ccf7fe5d51830b00419fd06bd6513d57c7a96588
namitamittal16 <44609518+namitamittal16@users.noreply.github.com>
2018-10-30 20:31:16 +0530

    updated typo and indentation

--- HellWorld.java
+++ HellWorld.java
@@ -1,7 +1,9 @@
-class Hello{
-  public static void main(String args[]){
+class Hello
+{
+  public static void main(String args[])
+  {
     System.out.println("Hello world!");
-    System.out.println("Hello Hactoberfest!");
+    System.out.println("Hello Hacktoberfest!");
     
   }
 }
