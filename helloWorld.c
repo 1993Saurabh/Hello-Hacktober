6ecb96c0ac927806074554ad49d5d24d58d0ba3c
surekhasangle <44473965+surekhasangle@users.noreply.github.com>
2020-10-04 23:28:34 +0530

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -1,3 +1,15 @@
+// hello world in c
 #include <stdio.h>
 
 void main() {printf("Hello, World!");}
+
+// hello world in cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  cout << " HELLO WORLD! " << endl;
+  
+return 0;
+}
